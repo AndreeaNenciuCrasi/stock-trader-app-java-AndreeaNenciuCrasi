466e24b36ee6fa97b0646213f5fd42a8d0b72e65
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-09-02 16:31:48 +0300

    Code with error

--- src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
+++ src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
@@ -1,16 +1,33 @@
 package com.codecool.stockApp;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 // TODO
 class StockAPIServiceTest {
 
     @Test // everything works
-    void testGetPriceNormalValues() {
+    void testGetPriceNormalValues() throws IOException {
+        RemoteURLReader reader = mock(RemoteURLReader.class);
+
+        when(reader.readFromUrl("https://run.mocky.io/v3/9e14e086-84c2-4f98-9e36-54928830c980?stock=zzapl")
+        ).thenReturn("{'price':338.85}");
+
+        StockAPIService service = new StockAPIService();
+        service.setRemoteURLReader(reader);
+
+        assertEquals(338.85, service.getPrice("zzapl"));
+
 
     }
+
+
 
     @Test // readFromURL threw an exception
     void testGetPriceServerDown() {
